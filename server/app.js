9d07a741ed0dd1c1564a72d129b0175018935c7a
Arne Berner <aberner@practical.no>
2025-06-09 22:13:04 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// API routes
 app.use('/api/scores', require('./routes/scores'));
 
-// Serve client in production (optional)
-// const path = require('path');
-// app.use(express.static(path.join(__dirname, '../client/build')));
-// app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../client/build/index.html')));
+// Serve React static files (production build)
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
